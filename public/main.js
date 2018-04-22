6b9dce7d233917f51b4bef3898eb87a2b1e00309
shahafWebiks <shahaf@webiks.com>
2018-04-22 12:22:50 +0300

    css fix

--- public/main.js
+++ public/main.js
@@ -7,7 +7,8 @@ const ACCESS_TOKEN = 'pk.eyJ1Ijoic2hhaGFmYW1zIiwiYSI6ImNqZjhpYWw4ZzB4Zjg0MG1xcHJ
 var mymap = L.map('mapid').setView([0, 0], 2);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+    'Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox.streets',
     accessToken: ACCESS_TOKEN
